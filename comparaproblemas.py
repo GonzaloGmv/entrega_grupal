1808039423957d24841297f09281a6b200a07181
tereesaalvarez <alvdeporteresa@gmail.com>
2021-12-14 19:18:03 +0100

    commit

--- comparaproblemas.py
+++ comparaproblemas.py
@@ -12,7 +12,18 @@ import sys
 # 2. INTEGER_ARRAY b
 #
 def compareTriplets(a, b):
-# Write your code here
+    puntosA = 0
+    puntosB = 0
+    for i in range(0,3):
+        if a[i] < b[i]:
+            puntosB += 1
+        elif a[i] > b[i]:
+            puntosA += 1
+
+    puntosTotales = [puntosA, puntosB]
+    return puntosTotales
+
+
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
     a = list(map(int, input().rstrip().split()))
