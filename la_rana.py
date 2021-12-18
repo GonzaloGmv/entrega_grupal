514f0792cbbacae7793c3d8b28cb69c273f22e6f
tereesaalvarez <alvdeporteresa@gmail.com>
2021-12-18 12:30:08 +0100

    clase tunel

--- la_rana.py
+++ la_rana.py
@@ -14,3 +14,9 @@ class coordenadas:
         else:
             return False
 
+class Tunel:
+    def __init__(self, x1, y1, x2, y2):
+        self.extremo1 = Coordenadas(x1,y1)
+        self.extremo2 = Coordenadas(x2,y2)
+
+def buscatunel(Casillax, Casillay, tuneles):
