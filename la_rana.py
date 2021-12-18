951fe768120b6dcd4521ea207a9c9d8add976a97
tereesaalvarez <alvdeporteresa@gmail.com>
2021-12-18 12:33:58 +0100

    def buscatunel

--- la_rana.py
+++ la_rana.py
@@ -16,7 +16,18 @@ class coordenadas:
 
 class Tunel:
     def __init__(self, x1, y1, x2, y2):
-        self.extremo1 = Coordenadas(x1,y1)
-        self.extremo2 = Coordenadas(x2,y2)
+        self.extremo1 = coordenadas(x1,y1)
+        self.extremo2 = coordenadas(x2,y2)
 
 def buscatunel(Casillax, Casillay, tuneles):
+    coordenadas = coordenadas(Casillax, Casillay)
+    for t in tuneles:
+        if(t.extremo1.comparate(Casillax, Casillay) == True):
+            coordenadas.x = t.extremo2.x
+            coordenadas.y = t.extremo2.y
+            break
+        elif(t.extremo2.comparate(Casillax, Casillay) == True):
+            coordenadas.x = t.extremo1.x
+            coordenadas.y = t.extremo1.y
+            break
+        return coordenadas
