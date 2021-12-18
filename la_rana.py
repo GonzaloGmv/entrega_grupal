9b814e33727ae5f9ea14488616add1f5f8912151
tereesaalvarez <alvdeporteresa@gmail.com>
2021-12-18 16:21:38 +0100

    def exploración

--- la_rana.py
+++ la_rana.py
@@ -20,7 +20,7 @@ class Tunel:
         self.extremo2 = coordenadas(x2,y2)
 
 def buscatunel(Casillax, Casillay, tuneles):
-    coordenadas = coordenadas(Casillax, Casillay)
+    Coordenadas = coordenadas(Casillax, Casillay)
     for t in tuneles:
         if(t.extremo1.comparate(Casillax, Casillay) == True):
             coordenadas.x = t.extremo2.x
@@ -31,3 +31,34 @@ def buscatunel(Casillax, Casillay, tuneles):
             coordenadas.y = t.extremo1.y
             break
         return coordenadas
+
+def exploracion(Casillax, Casillay, laberinto, n, m, tuneles):
+    num = 0
+    den = 0
+    probabilidad = 0.0
+    if(Casillax > 0 and laberinto[Casillax - 1][Casillay] != "#"):
+        den += 1
+        if(laberinto[Casillax - 1][Casillay] == "%"):
+            num += 1
+    if(Casillax < n-1 and laberinto[Casillax + 1][Casillay] != "#"):
+        den += 1
+        if(laberinto[Casillax + 1][Casillay] == "%"):
+            num += 1
+    if(Casillay < m-1 and laberinto[Casillax][Casillay + 1] != "#"):
+        den += 1
+        if(laberinto[Casillax][Casillay + 1] == "%"):
+            num += 1
+    if(Casillay > 0 and laberinto[Casillax][Casillay - 1] != "#"):
+        den += 1
+        if(laberinto[Casillax][Casillay - 1] == "%"):
+            num += 1
+    if (den == 0):
+        return probabilidad
+    probabilidad = num / den
+    
+    if(Casillax > 0 and laberinto[Casillax - 1][Casillay] == "$"):
+        laberintocopia = laberinto
+        coordenadas = buscatunel(Casillax - 1, Casillay, tuneles)
+        laberintocopia[Casillax][Casillay] = "#"
+        probabilidad += exploracion(coordenadas.x, coordenadas.y, laberintocopia, n, m, tuneles)/den
+        
